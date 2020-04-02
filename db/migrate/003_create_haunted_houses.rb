a50735971652a67a7dcb8253dd2abc6f315e43de
Beth Ruff <beth.ruff2@gmail.com>
2020-04-02 03:47:41 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,6 +6,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :theme
       t.float :price
       t.boolean :is_family_friendly
-      t.
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :long_description
+    end 
   end 
 end 
